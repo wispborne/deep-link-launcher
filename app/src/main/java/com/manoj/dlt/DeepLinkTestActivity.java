299badddc9ac5107b04e53f35080cceecf492569
Manoj <manoj@zophop.com>
2016-02-28 01:07:38 +0530

    testing changes

--- app/src/main/java/com/manoj/dlt/DeepLinkTestActivity.java
+++ app/src/main/java/com/manoj/dlt/DeepLinkTestActivity.java
@@ -1,20 +1,30 @@
 package com.manoj.dlt;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.*;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class DeepLinkTestActivity extends AppCompatActivity
 {
-    private EditText _deepLinkInput;
+    private AutoCompleteTextView _deepLinkInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -73,7 +83,12 @@ public class DeepLinkTestActivity extends AppCompatActivity
 
     private void initView()
     {
-        _deepLinkInput = (EditText) findViewById(R.id.deep_link_input);
+        _deepLinkInput = (AutoCompleteTextView) findViewById(R.id.deep_link_input);
+        List<String> array = Arrays.asList("zophop","zopnow","asdafasfda");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line,array);
+        _deepLinkInput.setAdapter(new AutoCompleteAdapter(this,android.R.layout.simple_dropdown_item_1line,array));
+        _deepLinkInput.setThreshold(0);
     }
 
     private boolean canResolve(Intent intent)
@@ -91,5 +106,27 @@ public class DeepLinkTestActivity extends AppCompatActivity
     private void raiseError(String errorText)
     {
         Toast.makeText(this, errorText, Toast.LENGTH_LONG).show();
+    }
+
+    private class AutoCompleteAdapter extends ArrayAdapter<String>
+    {
+        public AutoCompleteAdapter(Context context, int i, List<String> stringList)
+        {
+            super(context,i, stringList);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent)
+        {
+            TextView textView = new TextView(getContext());
+            String input = _deepLinkInput.getText().toString();
+            SpannableString inputHint = new SpannableString(input);
+            inputHint.setSpan(new ForegroundColorSpan(R.color.Blue), 0, inputHint.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            textView.setText(inputHint);
+            SpannableString remtext = new SpannableString(getItem(position).replace(input,""));
+            remtext.setSpan(new ForegroundColorSpan(R.color.Black), 0, remtext.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            textView.append(remtext);
+            return textView;
+        }
     }
 }
