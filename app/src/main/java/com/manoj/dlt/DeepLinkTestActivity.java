0e10fedb42efc0735fb39348e1178dc0901b523e
Manoj <manoj@zophop.com>
2016-02-27 20:42:27 +0530

    basic app functionality completed

--- app/src/main/java/com/manoj/dlt/DeepLinkTestActivity.java
+++ app/src/main/java/com/manoj/dlt/DeepLinkTestActivity.java
@@ -1,19 +1,27 @@
 package com.manoj.dlt;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class DeepLinkTestActivity extends AppCompatActivity
 {
+    private EditText _deepLinkInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deep_link_test);
+        initView();
     }
 
 
@@ -38,7 +46,50 @@ public class DeepLinkTestActivity extends AppCompatActivity
         {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void testDeepLink(View view)
+    {
+        String deepLinkUri = _deepLinkInput.getText().toString();
+        if (deepLinkUri.contains(":"))
+        {
+            Uri uri = Uri.parse(deepLinkUri);
+            Intent intent = new Intent();
+            intent.setData(uri);
+            intent.setAction(Intent.ACTION_VIEW);
+            if (canResolve(intent))
+            {
+                startActivity(intent);
+            } else
+            {
+                raiseError("No Activity found to resolve deep link");
+            }
+        } else
+        {
+            raiseError("Uri is improperly formed");
+        }
+    }
+
+    private void initView()
+    {
+        _deepLinkInput = (EditText) findViewById(R.id.deep_link_input);
+    }
+
+    private boolean canResolve(Intent intent)
+    {
+        PackageManager pm = getPackageManager();
+        if (pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY) != null)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    private void raiseError(String errorText)
+    {
+        Toast.makeText(this,errorText,Toast.LENGTH_LONG).show();
     }
 }
