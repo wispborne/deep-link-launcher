0d3ec4c8f7281530f72dddef834331d9a85feee9
Manoj <manoj@zophop.com>
2016-03-06 23:07:35 +0530

    removed commnet

--- app/src/main/java/com/manoj/dlt/ui/activities/DeepLinkTestActivity.java
+++ app/src/main/java/com/manoj/dlt/ui/activities/DeepLinkTestActivity.java
@@ -60,12 +60,7 @@ public class DeepLinkTestActivity extends AppCompatActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings)
         {
             return true;
