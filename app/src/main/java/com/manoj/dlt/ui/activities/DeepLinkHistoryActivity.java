2b615b1be30a7e3f83e384813c1870cbbc2abf12
Manoj <manoj@zophop.com>
2016-03-13 20:31:36 +0530

    deep link history activity added with basic layout

---
+++ app/src/main/java/com/manoj/dlt/ui/activities/DeepLinkHistoryActivity.java
@@ -0,0 +1,44 @@
+package com.manoj.dlt.ui.activities;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.manoj.dlt.R;
+
+public class DeepLinkHistoryActivity extends ActionBarActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_link_history);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_deep_link_history, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
