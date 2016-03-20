a4bb206089dba69355c553a96e68840863efbcf7
Manoj <manoj@zophop.com>
2016-03-20 14:12:27 +0530

    removed unused code

--- app/src/main/java/com/manoj/dlt/ui/activities/DeepLinkHistoryActivity.java
+++ app/src/main/java/com/manoj/dlt/ui/activities/DeepLinkHistoryActivity.java
@@ -131,31 +131,6 @@ public class DeepLinkHistoryActivity extends AppCompatActivity
         _adapter.updateBaseData(_history.getAllLinksSearchedInfo());
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_deep_link_history, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private boolean shouldFireDeepLink(int actionId, KeyEvent keyEvent)
     {
         if (actionId == EditorInfo.IME_ACTION_GO)
