b40177c7d4ee950cbd2370c388b98ebbf89a2672
Manoj <manoj@zophop.com>
2016-04-07 18:22:32 +0530

    key changed - previous history will be lost, overwritten by new one

--- app/src/main/java/com/manoj/dlt/Constants.java
+++ app/src/main/java/com/manoj/dlt/Constants.java
@@ -2,7 +2,7 @@ package com.manoj.dlt;
 
 public class Constants
 {
-    public static final String DEEP_LINK_HISTORY_KEY = "deep_link_history";
+    public static final String DEEP_LINK_HISTORY_KEY = "deep_link_history_key_v1";
     public static final String ONE_TIME_PREF_KEY = "one_time_key";
     public static final String APP_TUTORIAL_SEEN = "app_tut_seen";
 }
