4ce44c6bc77b32882646f3e071db395189bbe1d5
Manoj <manoj@zophop.com>
2016-06-05 21:37:09 +0530

    fix

--- app/src/main/java/com/manoj/dlt/services/FCMMessagingService.java
+++ app/src/main/java/com/manoj/dlt/services/FCMMessagingService.java
@@ -11,7 +11,9 @@ public class FCMMessagingService extends FirebaseMessagingService
 {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d("FCM","fcm messsage noti body = "+remoteMessage.getNotification().getBody());
+        if(remoteMessage.getNotification() != null) {
+            Log.d("FCM", "fcm messsage noti body = " + remoteMessage.getNotification().getBody());
+        }
         Log.d("FCM","fcm messsage data printing ");
         for(Map.Entry<String,String> entry: remoteMessage.getData().entrySet())
         {
