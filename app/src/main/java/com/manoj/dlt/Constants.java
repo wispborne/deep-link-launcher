04c2760838298038bfabd3ccc43039aaacffb679
Manoj <manoj@zophop.com>
2016-06-07 23:24:35 +0530

    refactor

--- app/src/main/java/com/manoj/dlt/Constants.java
+++ app/src/main/java/com/manoj/dlt/Constants.java
@@ -2,7 +2,7 @@ package com.manoj.dlt;
 
 public class Constants
 {
-    public enum ENVIRONMENT
+    public enum CONFIG
     {
         SANDBOX, PRODUCTION;
     }
@@ -10,12 +10,12 @@ public class Constants
     public static final String DEEP_LINK_HISTORY_KEY = "deep_link_history_key_v1";
     public static final String ONE_TIME_PREF_KEY = "one_time_key";
     public static final String APP_TUTORIAL_SEEN = "app_tut_seen";
+    public static CONFIG ENVIRONMENT  = CONFIG.SANDBOX;
 
-    private static ENVIRONMENT _environment = ENVIRONMENT.SANDBOX;
     private static final String USER_HISTORY_BASE = "https://sweltering-fire-2158.firebaseio.com/";
 
     public static String getUserHistoryBase()
     {
-        return USER_HISTORY_BASE.concat(_environment.name().toLowerCase()).concat("users");
+        return USER_HISTORY_BASE.concat(ENVIRONMENT.name().toLowerCase()).concat("users");
     }
 }
