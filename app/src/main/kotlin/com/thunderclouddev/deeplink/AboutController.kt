72b25ecfbddab706ad8199d84e6740402bf0b87a
David Whitman <whitmadm@gmail.com>
2017-01-29 17:38:28 +0100

    Fixed missing Back button on About view

--- app/src/main/kotlin/com/thunderclouddev/deeplink/AboutController.kt
+++ app/src/main/kotlin/com/thunderclouddev/deeplink/AboutController.kt
@@ -13,9 +13,14 @@ import com.thunderclouddev.deeplink.ui.BaseController
  */
 class AboutController(bundle: Bundle? = null) : BaseController(bundle) {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+        super.onCreateView(inflater, container)
         val binding = DataBindingUtil.inflate<AboutViewBinding>(inflater, R.layout.about_view, container, false)
         binding.model = ViewModel(BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE.toString())
         return binding.root
+    }
+
+    override fun getTitle(): String? {
+        return activity?.getString(R.string.menu_about_title)
     }
 
     internal data class ViewModel(val versionName: String,
