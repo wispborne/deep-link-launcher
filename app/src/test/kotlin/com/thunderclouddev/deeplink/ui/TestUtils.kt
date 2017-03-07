d3a54c6d4f85769b1c970ff0f229a2ed8b3a46b9
David Whitman <whitmadm@gmail.com>
2017-03-08 01:15:21 +0200

    Added test that handling apps change

---
+++ app/src/test/kotlin/com/thunderclouddev/deeplink/ui/TestUtils.kt
@@ -0,0 +1,7 @@
+package com.thunderclouddev.deeplink.ui
+
+import org.mockito.Mockito
+
+fun <T> anyKt(): T {
+    return Mockito.any<T>()
+}
--- app/src/test/kotlin/com/thunderclouddev/deeplink/ui/edit/EditLinkViewModelTest.kt
+++ app/src/test/kotlin/com/thunderclouddev/deeplink/ui/edit/EditLinkViewModelTest.kt
@@ -1,13 +1,19 @@
 package com.thunderclouddev.deeplink.ui.edit
 
 import android.databinding.Observable
+import android.databinding.ObservableArrayList
 import android.databinding.ObservableField
+import android.databinding.ObservableList
+import android.graphics.Bitmap
 import com.thunderclouddev.deeplink.BR
 import com.thunderclouddev.deeplink.data.CreateDeepLinkRequest
+import com.thunderclouddev.deeplink.ui.Uri
+import com.thunderclouddev.deeplink.ui.anyKt
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
@@ -18,6 +24,7 @@ import org.mockito.junit.MockitoJUnitRunner
 @RunWith(MockitoJUnitRunner::class)
 class EditLinkViewModelTest {
     @Mock lateinit var handlingAppsForUriFactory: HandlingAppsForUriFactory
+    @Mock lateinit var mockCallback: ObservableList.OnListChangedCallback<ObservableArrayList<AppViewModel>>
 
     lateinit var viewModel: EditLinkViewModel
 
@@ -33,7 +40,7 @@ class EditLinkViewModelTest {
         viewModel = EditLinkViewModel(createDeepLinkRequest, handlingAppsForUriFactory)
     }
 
-    @Test fun test_onCreate_updatesPreview() {
+    @Test fun `OnCreate updates preview`() {
         val onPropertyChangedCallback = mock(Observable.OnPropertyChangedCallback::class.java)
         viewModel.addOnPropertyChangedCallback(onPropertyChangedCallback)
 
@@ -43,34 +50,46 @@ class EditLinkViewModelTest {
         verify(onPropertyChangedCallback, never()).onPropertyChanged(viewModel, BR.authority)
     }
 
-    @Test fun test_onSchemeChanged_previewUpdates() {
+    @Test fun `Scheme change updates preview`() {
         viewModel.scheme.set("newScheme")
 
         Assert.assertEquals("newScheme://google.com", viewModel.getFullDeepLink())
     }
 
-    @Test fun test_onAuthorityChanged_previewUpdates() {
+    @Test fun `Authority change updates preview`() {
         viewModel.authority.set("new.auth")
 
         Assert.assertEquals("http://new.auth", viewModel.getFullDeepLink())
     }
 
-    @Test fun test_onPathChanged_previewUpdates() {
+    @Test fun `Path change updates preview`() {
         viewModel.path.set("1/2/3/4")
 
         Assert.assertEquals("http://google.com/1/2/3/4", viewModel.getFullDeepLink())
     }
 
-    @Test fun test_onQueryParamsChanged_previewUpdates() {
+    @Test fun `Query param change updates preview`() {
         viewModel.addQueryParam(ObservableField<String>("key1"), ObservableField<String>("value1"))
         viewModel.addQueryParam(ObservableField<String>("key2"), ObservableField<String>("value2"))
 
         Assert.assertEquals("http://google.com?key1=value1&key2=value2", viewModel.getFullDeepLink())
     }
 
-    @Test fun test_onFragmentChanged_previewUpdates() {
+    @Test fun `Fragment change updates preview`() {
         viewModel.fragment.set("newFragment")
 
         Assert.assertEquals("http://google.com#newFragment", viewModel.getFullDeepLink())
+    }
+
+    @Test fun `Preview change updates handling apps`() {
+        val listOfHandlers = listOf(AppViewModel("hello", "google", mock(Bitmap::class.java)))
+        `when`(handlingAppsForUriFactory.build(anyKt<Uri>(), ArgumentMatchers.eq(true))).thenReturn(listOfHandlers)
+        viewModel.handlingApps.addOnListChangedCallback(mockCallback)
+
+        viewModel.onCreate()
+        viewModel.scheme.set("newScheme")
+
+        verify(handlingAppsForUriFactory).build(anyKt(), ArgumentMatchers.anyBoolean())
+        verify(mockCallback).onItemRangeInserted(eq(ObservableArrayList<AppViewModel>().apply { addAll(listOfHandlers) }), eq(0), eq(1))
     }
 }
